858723c00160b36ba5b79554807b699fe6d1c2e0
Yasheed1995 <qwerty8608301@gmail.com>
2018-07-08 02:25:19 +0800

    second test

--- mmmo/app/src/main/java/com/example/logan1436/mmmo/CardsActivity.java
+++ mmmo/app/src/main/java/com/example/logan1436/mmmo/CardsActivity.java
@@ -2,6 +2,7 @@ package com.example.logan1436.mmmo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class CardsActivity extends AppCompatActivity {
     @Override
@@ -9,6 +10,6 @@ public class CardsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // set content view
         setContentView(R.layout.activity_cards);
-
+        Log.v("fjiej", "fejfiw");
     }
 }
